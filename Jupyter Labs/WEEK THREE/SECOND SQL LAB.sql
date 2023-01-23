6e94b4ef406b0085b9a3967def2e8e52ab104ee4
poppynevinadley <nevin.poppy@outlook.com>
2023-01-23 15:51:40 +0100

    Create SECOND SQL LAB.sql

---
+++ Jupyter Labs/WEEK THREE/SECOND SQL LAB.sql
@@ -0,0 +1,6 @@
+#Get the id values of the first 5 clients from district_id with a value equals to 1.
+#SELECT client_id from bank.district_id;
+SHOW TABLES FROM bank;
+SHOW FULL TABLES FROM bank;
+SELECT * FROM bank.district;
+SELECT district_id FROM bank.district;
