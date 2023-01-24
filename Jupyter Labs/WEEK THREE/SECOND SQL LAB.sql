4528c41312caf1edf1379aeb9e02c26572a7d55a
poppynevinadley <nevin.poppy@outlook.com>
2023-01-24 16:43:25 +0100

    Update SECOND SQL LAB.sql

--- Jupyter Labs/WEEK THREE/SECOND SQL LAB.sql
+++ Jupyter Labs/WEEK THREE/SECOND SQL LAB.sql
@@ -3,59 +3,121 @@ Use bank;
 #1. Get the id values of the first 5 clients from district_id with a value equals to 1. 
 SHOW TABLES FROM bank;
 SHOW FULL TABLES FROM bank;
-SELECT * FROM bank.district;
-SELECT client_id FROM client WHERE district_id = 1 LIMIT 5;
+SELECT 
+    *
+FROM
+    bank.district;
+SELECT 
+    client_id
+FROM
+    client
+WHERE
+    district_id = 1
+LIMIT 5;
 
-#2. In the client table, get an id value of the last client where the district_id equals to 72. 
-SELECT client_id
-FROM client 
-WHERE district_id = 72 
-ORDER BY client_id DESC LIMIT 1;
+SELECT 
+    client_id
+FROM
+    client
+WHERE
+    district_id = 72
+ORDER BY client_id DESC
+LIMIT 1;
 
-#3. Get the 3 lowest amounts in the loan table.                                 
-SELECT * FROM bank.loan;                                                   
-SELECT * FROM bank.loan ORDER BY amount ASC LIMIT 3;
+SELECT 
+    *
+FROM
+    bank.loan;
+SELECT 
+    *
+FROM
+    bank.loan
+ORDER BY amount ASC
+LIMIT 3;
 
-#4. What are the possible values for status, ordered alphabetically in ascending order in the loan table? 
-SELECT distinct status FROM bank.loan ORDER BY status;
+SELECT DISTINCT
+    status
+FROM
+    bank.loan
+ORDER BY status;
 
-#5. What is the loan_id of the highest payment received in the loan table?         
-SELECT loan_id FROM loan
-WHERE payments = (SELECT max(payments) FROM loan);
+SELECT 
+    loan_id
+FROM
+    loan
+WHERE
+    payments = (SELECT 
+            MAX(payments)
+        FROM
+            loan);
 
-#6. What is the loan amount of the lowest 5 account_ids in the loan table? Show the account_id and the corresponding amount.                                             
-SELECT * FROM loan ORDER BY account_id ASC LIMIT 5;
+SELECT 
+    *
+FROM
+    loan
+ORDER BY account_id ASC
+LIMIT 5;
 
-#7. What are the top 5 account_ids with the lowest loan amount that have a loan duration of 60 in the loan table?
-SELECT * FROM loan 
-WHERE duration = 60
-ORDER BY amount ASC LIMIT 5;
+SELECT 
+    *
+FROM
+    loan
+WHERE
+    duration = 60
+ORDER BY amount ASC
+LIMIT 5;
 
-#8. What are the unique values of k_symbol in the order table?                                               - TO CHECK
-#Note: There shouldn't be a table name order, since order is reserved from the ORDER BY clause. 
-#You have to use backticks to escape the order table name.
-SELECT DISTINCT k_symbol FROM 'order'; 
+SELECT 
+    *
+FROM
+    bank.order;
+SELECT DISTINCT
+    k_symbol
+FROM
+    bank.order
+ORDER BY k_symbol;
 
-#9. In the order table, what are the order_ids of the client with the account_id 34?						  - TO CHECK
-SELECT order_id 
-FROM 'order'
-WHERE account_id = 34;
+SELECT 
+    order_id
+FROM
+    bank.order
+WHERE
+    account_id = 34;
 
-#10. In the order table, which account_ids were responsible for orders between order_id 29540 and order_id 29560 (inclusive)? --STILL TO FINISH
-SELECT account_id
-FROM 'order'
-WHERE order id = order_id 29540 and order_id 29560;
-
-#11.In the order table, what are the individual amounts that were sent to (account_to) id 30067122? - -STILL TO FINISH
-SELECT amount
-FROM 'order'
-WHERE order_id = id 30067122;
+SELECT DISTINCT
+    account_id
+FROM
+    bank.order
+WHERE
+    order_id BETWEEN 29540 AND 29560;SELECT 
+    amount
+FROM
+    bank.order
+WHERE
+    account_to = 30067122;          #check whether have backticks                                  #strings need quotes
 
 #12. In the trans table, show the trans_id, date, type and amount of the 10 first transactions from account_id 793 in 
 #chronological order, from newest to oldest.                                                                     -STILL TO FINISH
-SELECT trans_id, date, type, amount
-FROM trans account_id 793
+SELECT trans_id, date, type, amount FROM trans account_id 793
 ORDER BY ASC LIMIT 10;
+
+#13.
+SELECT district_id, count(client_id) FROM client WHERE district_id <10 GROUP BY district_id
+ORDER BY 
+
+#17
+SELECT date, duration, count(load_id) FROM loan WHERE date between 971201 AND 971231
+GROUP BY date, duration GROUP BY date, duration; # some duplicates in there? 
+#like is string comparison, makes sure same i believe 
+
+#18.
+SELECT 'account_id', 'type', SUM('amount') AS 'total_amount' 
+FROM
+'trans'
+WHERE 'account_id' = 396
+GROUP 
+
+
 
 
 
