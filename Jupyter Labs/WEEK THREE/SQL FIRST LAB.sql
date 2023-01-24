2d3ee2327d18231fd65b3c7ef74c8ef71b86bee1
poppynevinadley <nevin.poppy@outlook.com>
2023-01-24 10:30:17 +0100

    Update SQL FIRST LAB.sql

--- Jupyter Labs/WEEK THREE/SQL FIRST LAB.sql
+++ Jupyter Labs/WEEK THREE/SQL FIRST LAB.sql
@@ -2,7 +2,7 @@
 SHOW FULL TABLES FROM sakila;
 SELECT * FROM sakila.actor;
 SELECT title from sakila.film;
-SELECT DISTINCT name as 'Language2'from sakila.language;
+SELECT DISTINCT name as 'Language'from sakila.language; #use sakila not need sakila on everything
 SELECT COUNT(STORE_ID) FROM sakila.store;
 SELECT COUNT(STAFF_ID) FROM sakila.staff;
 SELECT STAFF_ID from sakila.staff; #to check if above is correct
