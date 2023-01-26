c8462e911fc1ddd81d44480d5c47ceb112a03552
poppynevinadley <nevin.poppy@outlook.com>
2023-01-26 14:27:36 +0100

    Update Optional SQL Lab.sql

---
+++ Jupyter Labs/WEEK THREE/Optional SQL Lab.sql
@@ -0,0 +1,6 @@
+#1. Write a query to display for each store its store ID, city, and country.
+SELECT * from city;     #country = country_id | city = city, country_id, 
+select store.store_id, city.country_id, city_id
+	from staff 
+	join address 
+	USING (address_id);
